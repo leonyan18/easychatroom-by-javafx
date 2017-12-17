55205a1199e2355477b78da1482b8428cc3c238b
leonyan18 <1183503933@qq.com>
2017-12-17 20:50:10 +0800

    1
    new new

--- src/application/MyControllog.java
+++ src/application/MyControllog.java
@@ -1,30 +1,69 @@
 package application;
 
 import java.awt.TextArea;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import jdk.nashorn.internal.runtime.Scope;
 
-public class MyControllog {
+public class MyControllog implements Initializable{
+	private Socket socket=null;
 	static  Stage stage;
 	@FXML
 	private TextField user;
 	@FXML
+	private PasswordField pass;
+	@FXML
 	private Button login;
 	public void tomain() throws IOException {
-		if(user.getText().equals("121")) {
-			System.out.println("1213");
+		DataInputStream in=new DataInputStream(socket.getInputStream());
+		DataOutputStream out=new DataOutputStream(socket.getOutputStream());
+		out.writeInt(1);
+		out.writeUTF(user.getText());
+		System.out.println(pass.getText());
+		out.writeUTF(pass.getText());
+		System.out.println(pass.getText());
+		if(in.readInt()==1) {
 			stage.close();
 			stage=new Stage();
 			Main main=new Main();
 			main.start(stage);
+		}
+		else {
+			System.out.println("wrong");
+		}
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO 自动生成的方法存根
+		socket=new Socket();
+		if(!socket.isConnected()) {
+			InetAddress address;
+			try {
+				address = InetAddress.getByName("127.0.0.1");
+				InetSocketAddress socketAddress=new InetSocketAddress(address, 5555);
+				socket.connect(socketAddress);
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/severd/Server.java
+++ src/severd/Server.java
@@ -1,5 +1,17 @@
 package severd;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server=new ServerSocket(5555);
+		Socket you=null;
+		while(true) {
+			you=server.accept();
+			System.out.println("yes");
+			new Serverthread(you).start();
+		}
+	}
 }
