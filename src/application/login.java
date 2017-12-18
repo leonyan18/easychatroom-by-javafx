2628e64a883d82749a784004f1ef8e2595ee6438
leonyan18 <1183503933@qq.com>
2017-12-18 14:56:18 +0800

    1
    new

--- src/application/login.java
+++ src/application/login.java
@@ -3,8 +3,10 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import junit.framework.Test;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class login extends Application {
@@ -16,6 +18,7 @@ public class login extends Application {
 			MyControllog.stage=primaryStage;
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("resource/batman.jpg"));  
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
